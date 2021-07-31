6f7cfd99b888fe7a3c876c3077375364b44588bf
Andrii Trushevych <andrson451@gmail.com>
2021-07-31 17:45:58 +0300

    homework_6.1

--- djangoProject/education/students/views.py
+++ djangoProject/education/students/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from .models import Student
+
 from faker import Faker
+
+from .models import Student
 
 faker = Faker()
 
@@ -9,12 +11,18 @@ def index(request):
     return HttpResponse("""<p>Path: /generate-student/</p>
                            <p>Path: /generate-students/?count=value</p>
                            <p>Path: /teachers/</p>
+                           <p>Path: /teachers/?first_name=value&last_name=value&age=value</p>
                            <p>Path: /group/</p>""")
 
 
 def get_generate_student(request):
-    student = Student.objects.create(first_name=faker.first_name(), last_name=faker.last_name(), age=faker.random_int(18, 100))
-    return HttpResponse(f"{student.id}, {student.first_name}, {student.last_name}, {student.age}")
+    student = Student.objects.create(first_name=faker.first_name(),
+                                     last_name=faker.last_name(),
+                                     age=faker.random_int(18, 100))
+    return HttpResponse(f"{student.id}, "
+                        f"{student.first_name}, "
+                        f"{student.last_name}, "
+                        f"{student.age}")
 
 
 def get_generate_students(request):
@@ -23,7 +31,9 @@ def get_generate_students(request):
         if 0 < int(count) <= 100:
             output = ''
             for i in range(int(count)):
-                student = Student.objects.create(first_name=faker.first_name(), last_name=faker.last_name(), age=faker.random_int(18, 100))
+                student = Student.objects.create(first_name=faker.first_name(),
+                                                 last_name=faker.last_name(),
+                                                 age=faker.random_int(18, 100))
                 output += f"<p>{student.id}, {student.first_name}, {student.last_name}, {student.age}</p>"
             return HttpResponse(output)
         else:
--- djangoProject/education/teachers/views.py
+++ djangoProject/education/teachers/views.py
@@ -1,13 +1,45 @@
 from django.http import HttpResponse
-from .models import Teachers
+
 from faker import Faker
+
+from .models import Teachers
 
 faker = Faker()
 
 
 def get_teachers(request):
-    teachers = Teachers.objects.create(first_name=faker.first_name(), last_name=faker.last_name(),
-                                       subject=faker.random_element(elements=('math', 'physics', 'history', 'literature')))
-    teachers_list = Teachers.objects.all()
-    output = ''.join([f"<p>{t.first_name} {t.last_name}, {t.subject}</p>" for t in teachers_list])
-    return HttpResponse(f"{output}")
+    first_name = request.GET.get("first_name", None)
+    last_name = request.GET.get("last_name", None)
+    age = request.GET.get("age", None)
+    if first_name and last_name and age:
+        teachers_list = Teachers.objects.filter(first_name=first_name, last_name=last_name, age=age)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif first_name and last_name:
+        teachers_list = Teachers.objects.filter(first_name=first_name, last_name=last_name)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif first_name and age:
+        teachers_list = Teachers.objects.filter(first_name=first_name, age=age)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif last_name and age:
+        teachers_list = Teachers.objects.filter(last_name=first_name, age=age)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif first_name:
+        teachers_list = Teachers.objects.filter(first_name=first_name)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif last_name:
+        teachers_list = Teachers.objects.filter(last_name=last_name)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    elif age:
+        teachers_list = Teachers.objects.filter(age=age)
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
+    else:
+        teachers_list = Teachers.objects.all()
+        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        return HttpResponse(f"{output}")
