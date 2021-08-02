ffbb23a180b8e5482d27c9eb1bc8b58f3123af88
andrii500 <52294882+andrii500@users.noreply.github.com>
2021-08-02 17:02:56 +0300

    Update views.py
    rewrite get_teachers function to more compact

--- djangoProject/education/teachers/views.py
+++ djangoProject/education/teachers/views.py
@@ -8,35 +8,12 @@ faker = Faker()
 
 
 def get_teachers(request):
-    first_name = request.GET.get("first_name", None)
-    last_name = request.GET.get("last_name", None)
-    age = request.GET.get("age", None)
-    if first_name and last_name and age:
-        teachers_list = Teachers.objects.filter(first_name=first_name, last_name=last_name, age=age)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif first_name and last_name:
-        teachers_list = Teachers.objects.filter(first_name=first_name, last_name=last_name)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif first_name and age:
-        teachers_list = Teachers.objects.filter(first_name=first_name, age=age)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif last_name and age:
-        teachers_list = Teachers.objects.filter(last_name=first_name, age=age)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif first_name:
-        teachers_list = Teachers.objects.filter(first_name=first_name)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif last_name:
-        teachers_list = Teachers.objects.filter(last_name=last_name)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
-        return HttpResponse(f"{output}")
-    elif age:
-        teachers_list = Teachers.objects.filter(age=age)
+    args = request.GET
+    dict_param = {}
+    for key, value in args.items():
+        dict_param[key] = value
+    if dict_param:
+        teachers_list = Teachers.objects.filter(**dict_param)
         output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
         return HttpResponse(f"{output}")
     else:
