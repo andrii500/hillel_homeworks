0c6c9df90f39c8170b660cba231d1c7e8720621d
andrii500 <52294882+andrii500@users.noreply.github.com>
2021-08-03 15:28:24 +0300

    Update app.py
    correction of mistakes to homework 4

--- homework_4/app.py
+++ homework_4/app.py
@@ -1,37 +1,48 @@
 from create_db import Sales, app
+
 from flask import request
 
 
 @app.route("/")
 def index():
-    return f"""<p>Path: /summary</p>
-               <p>Path: /sales?product=Value1&payment_type=Value2</p>"""
+    return f"""<p>Path: /summary/</p>
+               <p>Path: /sales/</p>
+               <p>Path: /sales/?product=Value1</p>
+               <p>Path: /sales/?payment_type=Value2</p>
+               <p>Path: /sales/?product=Value1&payment_type=Value2</p>"""
 
 
 @app.route("/summary/")
 def get_summary():
     sales_ = Sales.query.all()
-
     date = ''
+
     for i in range(1, 32):
         day_price = 0
         for row in sales_:
             if row.transaction_date.day == i:
                 day_price += row.price
-
         date += f"<p>Transaction_date: 01/{i}/2009, day_price: {day_price}</p>"
+
     return date
 
 
-@app.route('/sales')
+@app.route('/sales/')
 def get_sales():
-    product = request.args.get('product')
-    payment_type = request.args.get('payment_type')
-
-    sales_ = Sales.query.filter_by(product=product, payment_type=payment_type).all()
+    args = request.args
+    dict_param = {}
     date = ''
 
-    for row in sales_:
-        date += f"<p>{row.id}, {row.transaction_date}, {row.product}, {row.price}, {row.payment_type}</p>"
+    for key, value in args.items():
+        dict_param[key] = value
+
+    if dict_param:
+        sales_ = Sales.query.filter_by(**dict_param).all()
+        for row in sales_:
+            date += f"<p>{row.id}, {row.transaction_date}, {row.product}, {row.price}, {row.payment_type}</p>"
+    else:
+        sales_ = Sales.query.all()
+        for row in sales_:
+            date += f"<p>{row.id}, {row.transaction_date}, {row.product}, {row.price}, {row.payment_type}</p>"
 
     return date
