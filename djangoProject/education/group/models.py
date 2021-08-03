363ef1db9dd55b179dd9c8a7aefb2874f5ed0db4
andrii500 <52294882+andrii500@users.noreply.github.com>
2021-08-03 15:47:22 +0300

    Delete djangoProject directory

--- djangoProject/education/group/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Group(models.Model):
-    title = models.CharField(max_length=100)
-    num_of_students = models.IntegerField(default=10)
-
-    def __str__(self):
-        return self.title
--- djangoProject/education/teachers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('teachers/', views.get_teachers, name='teachers'),
-]
