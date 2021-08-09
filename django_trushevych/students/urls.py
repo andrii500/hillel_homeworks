5a39a20fc199b7737a314d8d026c214b9f5342c1
andrson <andrson451@gmail.com>
2021-08-09 17:18:02 +0300

    homework 8

--- django_trushevych/students/urls.py
+++ django_trushevych/students/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('students/', views.get_students, name='get-students'),
     path('generate-student/', views.get_generate_student, name='generate-student'),
-    path('generate-students/', views.get_generate_students, name='generate-students')
+    path('generate-students/', views.get_generate_students, name='generate-students'),
+    path('create-student/', views.create_student_from_model, name='create-student'),
 ]
--- django_trushevych/teachers/views.py
+++ django_trushevych/teachers/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 from faker import Faker
 
-from .models import Teachers
+from .models import Teacher
+
+from .forms import TeacherFormFromModel
 
 faker = Faker()
 
@@ -13,10 +17,23 @@ def get_teachers(request):
     for key, value in args.items():
         dict_param[key] = value
     if dict_param:
-        teachers_list = Teachers.objects.filter(**dict_param)
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        teachers_list = Teacher.objects.filter(**dict_param)
+        output = ''.join([f"<p>{t.id}. {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
         return HttpResponse(f"{output}")
     else:
-        teachers_list = Teachers.objects.all()
-        output = ''.join([f"<p>{t.id}, {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
+        teachers_list = Teacher.objects.all()
+        output = ''.join([f"<p>{t.id}. {t.first_name} {t.last_name}, {t.age}, {t.subject}</p>" for t in teachers_list])
         return HttpResponse(f"{output}")
+
+
+def create_teacher_from_model(request):
+    if request.method == 'GET':
+        form = TeacherFormFromModel()
+    elif request.method == 'POST':
+        form = TeacherFormFromModel(request.POST)
+
+        if form.is_valid():
+            Teacher.objects.create(**form.cleaned_data)
+            return HttpResponse('Teacher created!')
+
+    return render(request, 'teacher_from_model.html', {'form': form})
